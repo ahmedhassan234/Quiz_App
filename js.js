e2a6e87762a2ab016e9e5f0b895124d9f8e4e734
Ahmed Hassan <99320311+ahmedhassan234@users.noreply.github.com>
2023-09-28 10:13:52 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,91 @@
+const quizData = [
+    {
+        question: "Which of the following can read and render HTML web pages",
+        a: "Server",
+        b: "Head Tak",
+        c: "empty",
+        d: "web browser",
+        correct: "d",
+    },
+    {
+        question: "Among the following operators identify the one which is used to allocate memory to array variables in JavaScript.",
+        a: " mew mallok",
+        b: "new ",
+        c: "alloc",
+        d: "malloc",
+        correct: "b",
+    },
+    {
+        question: "What does HTML stand for?",
+        a: "Hypertext Markup Language",
+        b: "Hypertext Markdown Language",
+        c: "Hyperloop Machine Language",
+        d: "Helicopters Terminals Motorboats Lamborginis",
+        correct: "a",
+    },
+    {
+        question: "Identify the incorrect HTML tag among the following",
+        a: "<input>",
+        b: "<list>",
+        c: "<select>",
+        d: "none of the above",
+        correct: "b",
+    },
+    {
+        question: "Identify the empty or void element in HTML.",
+        a: "<sup>",
+        b: "<br>",
+        c: "<p>",
+        d: "<abbr>",
+        correct: "b",
+    },
+];
+const quiz= document.getElementById('quiz')
+const answerEls = document.querySelectorAll('.answer')
+const questionEl = document.getElementById('question')
+const a_text = document.getElementById('a_text')
+const b_text = document.getElementById('b_text')
+const c_text = document.getElementById('c_text')
+const d_text = document.getElementById('d_text')
+const submitBtn = document.getElementById('submit')
+let currentQuiz = 0
+let score = 0
+loadQuiz()
+function loadQuiz() {
+    deselectAnswers()
+    const currentQuizData = quizData[currentQuiz]
+    questionEl.innerText = currentQuizData.question
+    a_text.innerText = currentQuizData.a
+    b_text.innerText = currentQuizData.b
+    c_text.innerText = currentQuizData.c
+    d_text.innerText = currentQuizData.d
+}
+function deselectAnswers() {
+    answerEls.forEach(answerEl => answerEl.checked = false)
+}
+function getSelected() {
+    let answer
+    answerEls.forEach(answerEl => {
+        if(answerEl.checked) {
+            answer = answerEl.id
+        }
+    })
+    return answer
+}
+submitBtn.addEventListener('click', () => {
+    const answer = getSelected()
+    if(answer) {
+       if(answer === quizData[currentQuiz].correct) {
+           score++
+       }
+       currentQuiz++
+       if(currentQuiz < quizData.length) {
+           loadQuiz()
+       } else {
+           quiz.innerHTML = `
+           <h2>You answered ${score}/${quizData.length} questions correctly</h2>
+           <button onclick="location.reload()">Reload</button>
+           `
+       }
+    }
+})
